68d4f957d4310ef26cae755fcf76bdef1cd1d8d5
wumbl3 <wumbl3@gmail.com>
2021-08-03 16:37:32 -0400

    Set up development

--- run_dev.sh
+++ run_dev.sh
@@ -2,15 +2,17 @@
 
 mkdir -p /tmp/nginx/
 
-pushd ./server
-source python_modules/bin/activate
-gunicorn szurubooru.facade:app --reload -b 127.0.0.1:6666 &
-nginx -p . -c nginx.dev.conf &
-popd
+fuser -k 6666/tcp & fuser -k 8001/tcp
 
-pushd ./client
+cd ./server
+. python_modules/bin/activate
+gunicorn szurubooru.facade:app --log-level 'debug' --reload -b 127.0.0.1:6666 &
+nginx -p . -c nginx.dev.conf &
+cd ..
+
+cd ./client
 pwd
 npm run watch &
-popd
+cd ..
 
 wait

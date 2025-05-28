e26fcebf5968c7c3c8b7f0fa6cccf0ca787c5f58
Michael Pinnegar <jazzepi@gmail.com>
2025-05-27 20:40:40 -0400

    Fix up build.js

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,8 @@ echo "rsyncing files to /boruu on the remote server"
 #export MOUNT_SQL='/mnt/booru_storage/MOUNTS/sql'
 #export PORT='9999' Not needed, using default
 
+docker compose build
+
 # Eventually add --delete-after to make it delete old files in the target directories
 # /home/jewel/.ssh/digital_ocean_id
 rsync -rvc \
@@ -22,8 +24,6 @@ rsync -rvc \
 . \
 root@booru:~/boruu
 
-docker-compose build
-
 docker save whitekitten-booru-client:latest | gzip | DOCKER_HOST=ssh://booru docker load
 docker save whitekitten-booru-server:latest | gzip | DOCKER_HOST=ssh://booru docker load
-ssh booru -t 'docker-compose -f /root/boruu/docker-compose.yml --env-file /dev/null up --force-recreate -d --no-build'
+ssh booru -t 'docker compose -f /root/boruu/docker-compose.yml --env-file /dev/null up --force-recreate -d --no-build'
